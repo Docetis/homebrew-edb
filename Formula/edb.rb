d6249e500387044293784428c5399f79d7ac8cb1
Luca Carnevale-Breguet <luca.carnevale-breguet@swisscom.com>
2025-12-01 17:13:15 +0100

    Bump edb to v0.0.3

--- Formula/edb.rb
+++ Formula/edb.rb
@@ -2,7 +2,7 @@ class Edb < Formula
   desc "eXist-DB CLI toolkit – export/import, watch-sync, XAR build, backup & rollback"
   homepage "https://github.com/Docetis/edb"
   url "https://github.com/Docetis/edb/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "84923b717522a5b6ede62b6e69c48c12c71f6c1015d367c48919530d59f1d19f"
+  sha256 "5fefa19c74fd0c644245defb0c22f5ca7e367230fe8b869b517af04b2d1a4cb5"
   license "MIT"
 
   depends_on "curl"
